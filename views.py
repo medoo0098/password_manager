9dea44f2f63c523b2c243d764c8f2e15723c8a7d
medoo0098 <m@hdi.cool>
2024-09-29 23:35:00 +0100

    login and register form was updated

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, url_for, redirect, session, flash
-from models import User, SiteEntry
+from models import User
 from config import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
@@ -12,7 +12,7 @@ from flask_login import (
 )
 from datetime import datetime
 from flask_bootstrap import Bootstrap5
-from forms import RegisterForm
+from forms import RegisterForm, LoginForm, LeaveForm
 
 
 def init_views(app):
@@ -22,21 +22,36 @@ def init_views(app):
     login_manager.login_view = "login"
     bootstrap5 = Bootstrap5(app)
 
+    year=datetime.now().year
+
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
 
-    @app.route("/")
+    @app.route("/", methods=("GET", "POST"))
     def home():
-        return render_template("index.html", title="Hire Intelligence Staff Portal", year=datetime.now().year)
+        form = LeaveForm()
+
+        return render_template("index.html", title="Hire Intelligence Staff Portal", year=datetime.now().year, form=form)
 
     @app.route("/login", methods=("GET", "POST"))
     def login():
-        return render_template("login.html", title="Login", year=datetime.now().year)
+        form = LoginForm()
+        username = form.username.data
+        password = form.password.data
+        return render_template("login.html", title="Login", year=year, form=form)
 
     @app.route("/register", methods=("GET", "POST"))
     def register():
         form = RegisterForm()
+        if form.validate_on_submit():
+            username = form.username.data
+            password = form.password.data
+            email = form.email.data
+            admin = form.admin.data
+            manager = form.manager.data
+            department = form.department.data
+            print(username, password, email, admin, manager, department)
         return render_template("register.html", title="Register", year=datetime.now().year, form=form)
 
     @app.route("/logout", methods=("GET", "POST"))
