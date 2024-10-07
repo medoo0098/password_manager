0f09e1b9972c28b3a564cb2d6f64a85a1fd36694
medoo0098 <111465979+medoo0098@users.noreply.github.com>
2024-10-07 17:15:36 +0100

    Licence table added

--- models.py
+++ models.py
@@ -60,4 +60,27 @@ class Overtime(db.Model):
     date = db.Column(db.DateTime(timezone=True))
 
 
+class Licence(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    date = db.Column(db.DateTime(timezone=True))
+    product_name = db.Column(db.String)
+    agreement = db.Column(db.String)
+    product_detail = db.Column(db.String)
+    usage = db.Column(db.String)
+    mac = db.Column(db.Boolean)
+    windows = db.Column(db.Boolean)
+    android = db.Column(db.Boolean)
+    ios = db.Column(db.Boolean)
+    unix = db.Column(db.Boolean)
+    portal_url = db.Column(db.String)
+    activation_key = db.Column(db.String)
+    username = db.Column(db.String)
+    password = db.Column(db.String)
+    purchased_by = db.Column(db.String)
+    purchased_date = db.Column(db.DateTime(timezone=True))
+    purchase_method = db.Column(db.String)
+    expiration_date = db.Column(db.DateTime(timezone=True))
+    auto_renew = db.Column(db.Boolean)
+    cost = db.Column(db.Float)
+    currency = db.Column(db.String)
 
